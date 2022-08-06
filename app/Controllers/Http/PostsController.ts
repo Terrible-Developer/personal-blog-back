f4d99008ed68377aa9319ff38e8e3b3ca3b20a22
wyvern800 <matheusguiferreira@hotmail.com>
2022-08-06 19:30:25 -0300

    Ordered posts list by 'created_at' flag

--- app/Controllers/Http/PostsController.ts
+++ app/Controllers/Http/PostsController.ts
@@ -1,149 +1,149 @@
 // import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-import { HttpContext } from '@adonisjs/http-server/build/standalone'
-import Database from '@ioc:Adonis/Lucid/Database'
-import Post from 'App/Models/Post'
-import User from 'App/Models/User';
-import PostLike from 'App/Models/PostLike'
-import { getQueryStrings } from '../../../utils';
+import { HttpContext } from "@adonisjs/http-server/build/standalone";
+import Database from "@ioc:Adonis/Lucid/Database";
+import Post from "App/Models/Post";
+import User from "App/Models/User";
+import PostLike from "App/Models/PostLike";
+import { getQueryStrings } from "../../../utils";
 
 export default class PostsController {
-    public async showAll(request: HttpContext) {
+  public async showAll(request: HttpContext) {
+    const params = getQueryStrings(request.response.request.url);
 
-      const params = getQueryStrings(request.response.request.url);
+    // Counts all the posts to set the total pages
+    const postCount = (await Post.all()).length;
+    const posts = await Database.from("posts")
+      .orderBy("created_at", "desc")
+      .paginate(params["page"], params["per_page"]);
 
-      // Counts all the posts to set the total pages
-      const postCount = (await Post.all()).length;
-      const posts = await Database.from("posts").paginate(params['page'], params['per_page']);
+    return { posts, totalPages: Math.ceil(postCount / params["per_page"]) };
+  }
 
-      return { posts, totalPages: Math.ceil(postCount / params['per_page'])};
-    }
+  public async showAllByUserId(request: HttpContext) {
+    const params = getQueryStrings(request.response.request.url);
 
-    public async showAllByUserId(request: HttpContext) {
+    const postCount = (
+      await Post.query().where("userid", request.params.userId)
+    ).length;
 
-        const params = getQueryStrings(request.response.request.url);
+    const userPosts = await Post.query()
+      .where("userid", request.params.userId)
+      .paginate(params["page"], params["per_page"]);
 
-        const postCount = (await Post.query().where('userid', request.params.userId)).length;
+    return { userPosts, totalPages: Math.ceil(postCount / params["per_page"]) };
+  }
 
-        const userPosts = await Post.query().where('userid', request.params.userId).paginate(params['page'], params['per_page']);
-
-        return { userPosts, totalPages: Math.ceil(postCount / params['per_page'])}
-    }
-
-    /*public async getFilteredPosts(request: HttpContext) {
+  /*public async getFilteredPosts(request: HttpContext) {
 
     }*/
 
-    public async show(request: HttpContext) {
-        const post = await Post.find(request.params.id)
-        return post
-    }
+  public async show(request: HttpContext) {
+    const post = await Post.find(request.params.id);
+    return post;
+  }
 
-    public async create(request: HttpContext) {
-        const params = request.request.requestBody
-        const postId = await Database
-            .table('posts')
-            .insert({
-                title: params.title,
-                content: params.content,
-                userid: params.userid,
-                likes_quantity: 0
-            })
-            .returning('id')
-        return postId
-    }
+  public async create(request: HttpContext) {
+    const params = request.request.requestBody;
+    const postId = await Database.table("posts")
+      .insert({
+        title: params.title,
+        content: params.content,
+        userid: params.userid,
+        likes_quantity: 0,
+      })
+      .returning("id");
+    return postId;
+  }
 
-    public async edit(request: HttpContext) {
-      const data = request.request.requestBody
+  public async edit(request: HttpContext) {
+    const data = request.request.requestBody;
 
-      const post = await Post.findOrFail(request.params.postId);
+    const post = await Post.findOrFail(request.params.postId);
 
-      // Update the post info
-      post.title = data.title;
-      post.content = data.content;
+    // Update the post info
+    post.title = data.title;
+    post.content = data.content;
 
-      await post?.save().then(() => {
-        return { "Success": "Post " + data.title + "successfully updated!" }
+    await post?.save().then(() => {
+      return { Success: "Post " + data.title + "successfully updated!" };
+    });
+  }
+
+  public async delete(request: HttpContext) {
+    const post = await Post.find(request.params.id);
+    await post
+      ?.delete()
+      .then(() => {
+        return "Post " + post.id + "successefuly deleted!";
+      })
+      .catch((e) => {
+        return e;
       });
+  }
+
+  public async likePost({ params, auth }) {
+    await auth.use("api").authenticate();
+    const user: User = await auth.use("api").user!;
+
+    let returnValue: boolean;
+    let existsLikes = await this.hasUserLikedPost({ params, auth });
+
+    if (!existsLikes) {
+      this.addLike(user.id.toString(), params.id);
+      this.updateLikes(params.id, "like");
+      returnValue = true;
+    } else {
+      this.removeLike(user.id.toString(), params.id);
+      this.updateLikes(params.id, "dislike");
+      returnValue = false;
     }
 
-    public async delete(request: HttpContext) {
-        const post = await Post.find(request.params.id)
-        await post?.delete().then(() => {
-            return 'Post ' + post.id + 'successefuly deleted!'
-        }).catch(e => {
-            return e
-        })
+    return returnValue;
+  }
 
+  public async hasUserLikedPost({ params, auth }) {
+    await auth.use("api").authenticate();
+    const user: User = await auth.use("api").user!;
+    const postId = params.id;
+
+    const hasLiked = await Database.query()
+      .from("posts_like_users")
+      .where("userid", "=", user.id)
+      .andWhere("postid", "=", postId);
+    const count = Object.keys(hasLiked).length;
+    if (count > 0) {
+      return true;
+    }
+    return false;
+  }
+
+  private async addLike(userId: string, postId: string) {
+    const postLike: PostLike = await PostLike.create({
+      postId: postId,
+      userId: userId,
+    });
+    return "success";
+  }
+
+  private async removeLike(userId: string, postId: string) {
+    const dbResponse = await Database.query()
+      .delete()
+      .from("posts_like_users")
+      .where("userid", userId)
+      .andWhere("postid", postId);
+    return dbResponse;
+  }
+
+  private async updateLikes(postId: string, type: string) {
+    const post = await Post.findOrFail(postId);
+
+    // Process the like or dislike
+    if (type === "like") {
+      post.likes_quantity++;
+    } else {
+      if (post.likes_quantity > 0) post.likes_quantity--;
     }
 
-    public async likePost({ params, auth }){
-        await auth.use('api').authenticate()
-        const user: User = await auth.use('api').user!
-
-        let returnValue: boolean
-        let existsLikes = await this.hasUserLikedPost({params, auth})
-
-        if(!existsLikes){
-            this.addLike(user.id.toString(), params.id)
-            this.updateLikes(params.id, 'like');
-            returnValue = true
-        }
-        else{
-            this.removeLike(user.id.toString(), params.id)
-            this.updateLikes(params.id, 'dislike');
-            returnValue = false
-        }
-
-        return returnValue
-
-    }
-
-    public async hasUserLikedPost({ params, auth }){
-        await auth.use('api').authenticate()
-        const user: User = await auth.use('api').user!
-        const postId = params.id
-
-        const hasLiked = await Database
-            .query()
-            .from('posts_like_users')
-            .where('userid', '=', user.id)
-            .andWhere('postid', '=', postId)
-        const count = Object.keys(hasLiked).length
-        if(count > 0){
-            return true
-        }
-        return false
-    }
-
-    private async addLike(userId: string, postId: string){
-        const postLike: PostLike = await PostLike.create({
-            postId: postId,
-            userId: userId
-        })
-        return 'success'
-    }
-
-    private async removeLike(userId: string, postId: string){
-        const dbResponse = await Database
-            .query()
-            .delete()
-            .from('posts_like_users')
-            .where('userid', userId)
-            .andWhere('postid', postId)
-        return dbResponse
-    }
-
-    private async updateLikes(postId: string, type: string) {
-      const post = await Post.findOrFail(postId)
-
-      // Process the like or dislike
-      if (type === 'like') {
-        post.likes_quantity++;
-      } else {
-        if (post.likes_quantity > 0)
-          post.likes_quantity--;
-      }
-
-      post.save()
-    }
+    post.save();
+  }
 }
